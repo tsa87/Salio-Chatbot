c2a7689be3f47c0ff0b4c114d939127e297ec908
Tony <tsa87@sfu.ca>
2020-05-06 15:05:01 -0700

    update

--- salio.py
+++ salio.py
@@ -1,25 +1,80 @@
 import sister
 import pickle
-import os.path
+import os
+import glob
+import json
+import re
 import random
 import numpy as np
+
 sentence_embedding = sister.MeanEmbedding(lang="en")
 
 def welcome_message():
     print('           _______ _______        _____  _____ ')
     print('           |______ |_____| |        |   |     |')
     print('           ______| |     | |_____ __|__ |_____|')
-welcome_message()
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 class Salio():
     def __init__(self):
         self.last_sentence = "hi"
         self.learn = False
+
         try:
-            self.knowledge = pickle.load( open( "save.p", "rb" ) )
+            self.load_pickle("save.p")
         except:
             self.knowledge = []
-            print("FALED TO LOAD")
+
+    def start(self):
+        welcome_message()
+        while True:
+            try:
+                print(">>>: ", end="")
+                convo = input()
+                print()
+
+                if convo == "bye":
+                    print('bye')
+                    self.save()
+                    break
+
+                if convo == '$list':
+                    for known in self.knowledge:
+                        print(known[1])
+
+                if convo == "$load":
+                    num_loaded = 0
+                    files = glob.glob('messages/**/*.json', recursive= True)
+                    print(files)
+                    total_files = len(files)
+
+                    for file in files:
+                        self.load_json(file)
+                        num_loaded += 1
+                        print(f"{num_loaded} out of {total_files} loaded")
+
+                    print(f'{num_loaded} files loaded.')
+                    self.save()
+                    continue
+
+                if convo == "$learn":
+                    self.learn = False if self.learn else True
+                    continue
+
+                self.addResponse(self.last_sentence, convo)
+                reply = self.reply(convo)
+                if reply == "...":
+                    self.last_sentence = convo
+                else:
+                    self.last_sentence = reply
+
+                print(f'Salio: {reply}\n')
+
+            except:
+                continue
 
 
     def addResponse(self, condition, response):
@@ -28,44 +83,44 @@ class Salio():
 
     def reply(self, condition):
         vectorized_condition = sentence_embedding(condition)
+        dist = []
         for experience in self.knowledge:
-            if np.allclose(vectorized_condition, experience[0], atol=0.05):
-                return experience[1]
-        if self.learn:
+            dist.append(np.linalg.norm(vectorized_condition - experience[0]))
+        min_dist, i = min((val, i) for (i, val) in enumerate(dist))
+
+        if self.learn and min_dist > 0.1:
             return '...'
-        else:
-            return random.choice(self.knowledge)[1]
+
+        prob = softmax(dist)
+        index = dist.index(np.random.choice(dist, p=prob))
+        return self.knowledge[index][1]
+
+    def save(self):
+        print("saving")
+        with open("save.p", 'wb') as file:
+            pickle.dump(self.knowledge, file)
+
+    def load_pickle(self, path):
+        self.knowledge = pickle.load(open(path, "rb" ))
+
+    def load_json(self, path):
+        messages = json.load(open(path, 'r'))["messages"]
+
+        total_messages = len(messages)
+        num_loaded = 0
+
+        last_convo = "hey"
+        for i in range(total_messages-1, -1, -1):
+            message = messages[i]
+            if "content" in message:
+                content = re.sub(r"[^a-zA-Z0-9]+", ' ', message["content"]).strip()
+                if not content == "":
+                    salio.addResponse(last_convo, content)
+                    last_convo = content
+            num_loaded += 1
+            if (num_loaded % 100) == 0:
+                print(f'{num_loaded} out of {total_messages} loaded')
+                self.save()
 
 salio = Salio()
-
-while True:
-    print(">>>: ", end="")
-    convo = input()
-    if convo == "bye":
-        print('bye')
-        with open("save.p", 'wb') as file:
-            pickle.dump(salio.knowledge, file)
-        break
-
-    if convo == "$load":
-        with open("data.txt", "r") as file:
-            lines = file.readlines()
-            last_convo = "hey"
-            for line in lines:
-                convo = " ".join(line.strip().split(" "))
-                salio.addResponse(last_convo, convo)
-                last_convo = convo
-        continue
-
-    if convo == "$learn":
-        salio.learn = False if salio.learn else True
-        continue
-
-    salio.addResponse(salio.last_sentence, convo)
-    reply = salio.reply(convo)
-    if reply == "...":
-        salio.last_sentence = convo
-    else:
-        salio.last_sentence = reply
-
-    print(f'Salio: {reply}')
+salio.start()
